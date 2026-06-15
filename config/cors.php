8bd89bee85136fd9939d59e7040871f619495b00
zhafrandzaky <zhafrandzaky0304@gmail.com>
2026-06-16 05:14:57 +0800

    fix: support multiple CORS origins via CORS_ALLOWED_ORIGINS env

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173'))),
 
     'allowed_origins_patterns' => [],
 
